74d59153f81235e5134070dc63ee92e1b0b2524a
Jonathan Wax <jonathan@netusup.com>
2015-08-22 10:59:43 +0300

    cleanup - removed test module

--- desktop/app/app.js
+++ desktop/app/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    angular.module('app', ['common', 'shell', 'home', 'help', 'test']);
+    angular.module('app', ['common', 'shell', 'home', 'help']);
 
     // CONFIG: App (module)
     angular
